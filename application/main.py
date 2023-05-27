fba579e58e1adb0f0b7c37e7d95b2381c3ef0541
shun198 <shunhiroseluvmri@gmail.com>
2023-05-27 17:13:58 +0900

    👍 update Dockerfile

--- application/main.py
+++ application/main.py
@@ -1,5 +1,10 @@
 from selenium import webdriver
+import os
+import signal
 
-browser = webdriver.Firefox()
-browser = webdriver.Chrome()
-from PIL import image
+# https://qiita.com/shobota/items/5324ca810b3848c4fd0f
+try:
+    browser = webdriver.Chrome()
+    browser.get("https://scraping-for-beginner.herokuapp.com/login_page")
+finally:
+    os.kill(browser.service.process.pid, signal.SIGTERM)
